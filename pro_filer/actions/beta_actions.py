85bcd5cafd0b55ce47054df019a2c51e1a2b0a78
gabrielaguiardantas <gabrielaguiardantas@yahoo.com.br>
2023-07-06 23:04:25 -0300

    req. 1 ok

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -1,11 +1,19 @@
 """Arquivo que estudantes devem editar"""
 
 
+def count_bar(string_path):
+    count = 0
+    for char in string_path:
+        if char == "/":
+            count += 1
+    return count
+
+
 def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = max(context["all_files"], key=count_bar)
         print(f"Deepest file: {deepest_file}")
 
 
